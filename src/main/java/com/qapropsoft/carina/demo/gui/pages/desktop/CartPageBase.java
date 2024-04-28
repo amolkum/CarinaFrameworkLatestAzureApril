968cf24a22f527e9e1b62f6819e6bb3d10d63a37
Amol Kumbhare <akumbhare+akumbhare@users.noreply.github.com>
2024-04-29 02:18:41 +0530

    First commit

---
+++ src/main/java/com/qapropsoft/carina/demo/gui/pages/desktop/CartPageBase.java
@@ -0,0 +1,20 @@
+package com.qapropsoft.carina.demo.gui.pages.desktop;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+
+import com.zebrunner.carina.webdriver.gui.AbstractPage;
+//import com.qaprosoft.carina.core.gui.AbstractPage;
+
+public abstract class CartPageBase extends AbstractPage {
+
+	protected CartPageBase(WebDriver driver) {
+		super(driver);
+	}
+
+	public abstract CartPageBase productGetTextName(String s1);
+
+	public abstract CartPageBase productGetProductQuantity(String q1);
+
+}
